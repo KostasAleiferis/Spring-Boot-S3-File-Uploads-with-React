9c903ab39a9d9164521460df4e314e7b8a0e2202
Kostas Aleiferis <k.aleiferis@i-docs.com>
2024-11-19 17:04:43 +0200

    add spring-boot project

---
+++ src/test/java/com/kostas/aws_image_upload/AwsImageUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kostas.aws_image_upload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsImageUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
